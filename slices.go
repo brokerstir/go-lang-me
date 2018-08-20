8eec6537ed81c4d871c60feba81d1a33806766b3
Robert Ris <take1risk@gmail.com>
2018-08-20 13:33:23 -0500

    append to slice

--- slices.go
+++ slices.go
@@ -9,4 +9,8 @@ func main() {
     fmt.Println(a, s1, s2)
     fmt.Println("len(s1):", len(s1), "cap(s1):", cap(s1))
     fmt.Println("len(s2):", len(s2), "cap(s2):", cap(s2))
+    s2 = append(s2, 5)
+    fmt.Println(a, s1, s2)
+    fmt.Println("len(s1):", len(s1), "cap(s1):", cap(s1))
+    fmt.Println("len(s2):", len(s2), "cap(s2):", cap(s2))
 }
