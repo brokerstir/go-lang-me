8089074b7bb1724dbb76ecaa9b94cd38b7d3c3fb
Robert Ris <take1risk@gmail.com>
2018-08-20 10:29:51 -0500

    pointer as funcition value

--- pointers.go
+++ pointers.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var aValue float64 = 1.23
-	var aPointer *float64 = &aValue
-	fmt.Println("aPointer:", aPointer)
-	fmt.Println("*aPointer:", *aPointer)
+	myNumber := 2.6
+	halve(myNumber)                              // Does nothing!
+	fmt.Println("myNumber in 'main':", myNumber) // Prints 2.6!
+}
+
+func halve(number float64) {
+	number = number / 2
+	fmt.Println("number in 'halve':", number) // Prints 1.3, but change is only effective here!
 }
