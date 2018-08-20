1528b98b1aedc076b685e4487a5bfe6ccd863e37
Robert Ris <take1risk@gmail.com>
2018-08-20 13:22:26 -0500

    info on slices

--- slices.go
+++ slices.go
@@ -6,7 +6,7 @@ func main() {
     a := [5]int{0, 1, 2, 3, 4}
     s1 := a[0:3]
     s2 := a[2:5]
-    s1 = s1[0:4]
-    s2 = s2[0:4]
     fmt.Println(a, s1, s2)
+    fmt.Println("len(s1):", len(s1), "cap(s1):", cap(s1))
+    fmt.Println("len(s2):", len(s2), "cap(s2):", cap(s2))
 }
