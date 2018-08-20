5a0a2be7d0aca7594b1cc8bcef1a4a953245055b
Robert Ris <take1risk@gmail.com>
2018-08-20 13:47:51 -0500

    loop through slice

--- slices.go
+++ slices.go
@@ -3,12 +3,8 @@ package main
 import "fmt"
 
 func main() {
-    a := [5]int{0, 1, 2, 3, 4}
-    s1 := a[0:3]
-    s2 := a[2:5]
-    s2[0] = 88
-    s2 = append(s2, 5)
-    fmt.Println(a, s1, s2)
-    s2[0] = 999
-    fmt.Println(a, s1, s2)
+    s := []int{1, 2, 3}
+    for i, v := range s {
+      fmt.Println("element:", i, "value:", v)
+    }
 }
