fdbed24628996ecc8aeb70ba2089751d99e6dae5
Robert Ris <take1risk@gmail.com>
2018-08-20 11:20:49 -0500

    iterate arrays range loop

--- arrays.go
+++ arrays.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
     months := [3]string{"Apr", "May", "Jun"}
     salesByMonth := [3]float64{1710.26, 2245.97, 3032.40}
-    for i := 0; i < len(months); i++{
-        fmt.Println(months[i], salesByMonth[i])  
+    for i, month := range months{
+        fmt.Println(month, salesByMonth[i])  
     }
 }
