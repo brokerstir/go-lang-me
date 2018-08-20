3a77207fdc8844e7c4936651755b110869bd0e58
Robert Ris <take1risk@gmail.com>
2018-08-20 13:38:31 -0500

    evidence in change of underlying array

--- slices.go
+++ slices.go
@@ -6,11 +6,9 @@ func main() {
     a := [5]int{0, 1, 2, 3, 4}
     s1 := a[0:3]
     s2 := a[2:5]
-    fmt.Println(a, s1, s2)
-    fmt.Println("len(s1):", len(s1), "cap(s1):", cap(s1))
-    fmt.Println("len(s2):", len(s2), "cap(s2):", cap(s2))
+    s2[0] = 88
     s2 = append(s2, 5)
     fmt.Println(a, s1, s2)
-    fmt.Println("len(s1):", len(s1), "cap(s1):", cap(s1))
-    fmt.Println("len(s2):", len(s2), "cap(s2):", cap(s2))
+    s2[0] = 999
+    fmt.Println(a, s1, s2)
 }
