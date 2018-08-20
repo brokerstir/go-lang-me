83f52bd912e32707f39e05a7e21ee87929d0cc98
Robert Ris <take1risk@gmail.com>
2018-08-20 15:08:37 -0500

    map with loop blank identifier

--- map.go
+++ map.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 func main() {
-  ages := map[string]float64{}
+  ages := map[string]float64{"Alice": 12, "Carol": 10, "Bob": 9}
   // Like the array/slice syntax, but you can use any value of the type you specified for the keys.
-  ages["Alice"] = 12
-  ages["Bob"] = 9
-  fmt.Println(ages)
+  for _, age := range ages {
+  	fmt.Println(age)
+  }
 }
+
