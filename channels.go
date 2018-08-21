6325eb63ee13491cef3e728368807d1d5f3de74d
Robert Ris <take1risk@gmail.com>
2018-08-21 14:23:01 -0500

    channels

--- channels.go
+++ channels.go
@@ -7,7 +7,7 @@ import (
 )
 
 func longTask(channel chan int) {
-	delay := rand.Intn(5)
+	delay := rand.Intn(7)
 	fmt.Println("Starting long task")
 	time.Sleep(time.Duration(delay) * time.Second)
 	fmt.Println("Long task finished")
@@ -17,6 +17,10 @@ func longTask(channel chan int) {
 func main() {
 	rand.Seed(time.Now().Unix())
   channel := make(chan int)
-	go longTask(channel)
-	fmt.Println("Took", <-channel, "seconds")
+  for i := 1; i <= 3; i++ {
+    go longTask(channel)
+  }
+	for i := 1; i <= 3; i++ {
+	 fmt.Println("Took", <-channel, "seconds")
+  }
 }
