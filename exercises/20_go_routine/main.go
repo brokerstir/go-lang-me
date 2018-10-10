70056ffb6f240c25d085477f82e2e2f5041d5c54
Robert Ris <take1risk@gmail.com>
2018-10-10 07:38:31 -0500

    add more exercises

---
+++ exercises/20_go_routine/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	fmt.Println("OS\t\t", runtime.GOOS)
+	fmt.Println("ARCH\t\t", runtime.GOARCH)
+	fmt.Println("CPUs\t\t", runtime.NumCPU())
+	fmt.Println("Goroutines\t", runtime.NumGoroutine())
+
+	go foo()
+	//foo()
+	bar()
+
+	fmt.Println("CPUs\t\t", runtime.NumCPU())
+	fmt.Println("Goroutines\t", runtime.NumGoroutine())
+}
+
+func foo() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("foo:", i)
+	}
+}
+
+func bar() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("bar:", i)
+	}
+}
